ca88d654aeff9bdaa5cb5298bb98449c5056d361
Tom_Pascarella <paskarelkarel@gmail.com>
2024-09-23 08:30:34 +0200

    first commit

---
+++ git_config.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+git config --global user.name "Tom_Pascarella"
+git config --global user.email "paskarelkarel@gmail.com"
+echo "Přihlášení uživatele proběhlo v pořádku"
+# cd tisic
+# php artisan serve
