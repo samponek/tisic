b9a0361b406941a9411d098b1392482c62880628
Tom_Pascarella <paskarelkarel@gmail.com>
2024-09-24 08:09:26 +0200

    feat: :bug: coolboy

--- git_config.sh
+++ git_config.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+composer create-project laravel/laravel frajirek
+git config --list
 git config --global user.name "Tom_Pascarella"
 git config --global user.email "paskarelkarel@gmail.com"
 echo "Přihlášení uživatele proběhlo v pořádku"
+ls
+dir
+
 # cd tisic
 # php artisan serve
